ad4bc1088a19c1c739607d32a31d549953c74840
HINOJOSA PINTO <sahipin@dsic.upv.es>
2020-09-16 19:23:26 +0200

    files

--- webgl/js/clickPuntos.js
+++ webgl/js/clickPuntos.js
@@ -12,7 +12,7 @@ var VSHADER_SOURCE =
 
 var FSHADER_SOURCE =
     'void main(){            			          \n' +
-    '  gl_FragColor = vec4(0.0,0.5,0.5,1.0);      \n' +
+    '  gl_FragColor = vec4(0.3,0.0,0.5,0.1);      \n' +
     '}                        				      \n';
 
 
@@ -31,14 +31,16 @@ function main() {
         return;
     }
 
+    gl.clearColor(1, 0.0, 0.0, 1.0);
+	
     // Cargar, compilar y montar los shaders en un 'program'
     if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-        console.log("Se ha liado en la carga de los shaders")
+        console.log("no se ha realizado la carga de los shaders")
         return;
     }
 
-    gl.clearColor(0.0, 0.0, 0.3, 1.0);
     gl.clear(gl.COLOR_BUGGET_BIT);
+	
     var coordenadas = gl.getAttribLocation(gl.program, 'posicion');
 
     canvas.onmousedown = function (evento) {
@@ -47,27 +49,27 @@ function main() {
 }
 
 var puntos = []; //Array de puntos
+
 function click(evento, gl, canvas, coordenadas) {
     // p.dot = d.vector*[x,y] = g.vector*[x',y'] = g.vector*A*[x,y] Que es A?
     var x = evento.clientX;
     var y = evento.clientY;
     var rect = evento.target.getBoundingClientRect();
 
-    //Conversion de cordenadas
+    //Conversion de cordenadas al sistema de WebGl por defecto
+	//cuadrado de 2x2 centrado
     x = ((x - rect.left) - canvas.width / 2) * 2 / canvas.width;
     y = (canvas.height / 2 - (y - rect.top)) * 2 / canvas.height;
 
     //Guardar los puntos
-    puntos.push(x);
-    puntos.push(y);
+    puntos.push(x); puntos.push(y);
 
     //Borrar el canvas
     gl.clear(gl.COLOR_BUGGET_BIT);
 
-    //Inserta las coodenadas de los puntos como atributos y los dibuja uno a uno
-
+    //Inserta las coodenadas de los puntos como atributos y dibujarlos uno a uno
     for (var i = 0; i < puntos.length; i += 2) {
-        gl.vertexAttrib3f(coordenadas, puntos[i], puntos[i + 1], 0.0);
+        gl.vertexAttrib3f(coordenadas, puntos[i], puntos[i + 1], 0.3);
         gl.drawArrays(gl.POINTS, 0, 1);
     }
 
