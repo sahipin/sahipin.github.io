b79983c501fb159845f8e45c5d5a564203b7d7a4
sahipin <sahipin@alumni.upv.es>
2020-10-20 21:08:53 +0200

    Evolutivo.

--- webgl/threejs/evolutive.js
+++ webgl/threejs/evolutive.js
@@ -220,7 +220,7 @@ function generaCiudad() {
     }
 		tex_to_modify.wrapS = THREE.RepeatWrapping;
 		tex_to_modify.wrapT = THREE.RepeatWrapping;
-		tex_to_modify.repeat.set( tokens[4], height );
+		tex_to_modify.repeat.set( 1, height );
 
     var building = new THREE.Mesh(cube_building, material);
     building.position.x = separation_dist * tokens[0]*1+tokens[3]/2+0.5;
