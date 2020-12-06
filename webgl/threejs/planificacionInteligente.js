0560f9cf1b101f9396eb277306186d54a772a9f4
sahipin@alumni.upv.es <sahipin@alumni.upv.es>
2020-12-06 12:10:08 +0100

    plan

--- webgl/threejs/planificacionInteligente.js
+++ webgl/threejs/planificacionInteligente.js
@@ -7,8 +7,8 @@
 // Variables globales imprescindibles
 // motor render (dibujar), estructura datos almacen dibujos, desde donde dibujamos
 var renderer, scene, camera, cenital, personalCamera;
-var material_default;
-var tex_default;
+var materialE,materialG,materialF,materialR,materialS,materialH,material_default;
+var texE,texG,texF,texR,texS,texH,tex_default;
 // Variables globales
 var elevator0, elevator1, elevator2, elevator3, elevator4;
 var passenger0, passenger1, passenger2, passenger3, passenger4;
@@ -18,7 +18,7 @@ var l = b = -r;
 var max_height = 20, separation_dist = 1;
 var cameraController;
 var lineToRead = 0;
-var path = "images/ImagesPIN";
+var path = "images/";
 // Global GUI
 var effectController;
 var backX, backZ;
@@ -32,19 +32,21 @@ function setCameras(ar ){
 	//contruir las 4 cámaras
 
 	var origen = new THREE.Vector3(0,0,0);
+	/*
 	//ortográfica
 	var camOrtografica = new THREE.OrthographicCamera(l, r, t, b, -200, 200);
 	cenital = camOrtografica.clone();
 	cenital.position.set(0,130,0);
 	cenital.lookAt(origen);
 	cenital.up = new THREE.Vector3(0,0,-1);
+	*/
 
 	//perspectiva
-	camera = new THREE.PerspectiveCamera( 50, ar, 0.1, 10000 ); // valores de cerca y lejos (los dos ultimos)
+	camera = new THREE.PerspectiveCamera(45, ar, 0.1, 10000 ); // valores de cerca y lejos (los dos ultimos)
     // Movemos la camare respecto al sistema de referencia de la scena
-  camera.position.set(125, 140, 250); // traslado de la camara desde el origen de coordenadas
-  camera.lookAt(new THREE.Vector3(0,0,0));
-  //scene.add(cenital);
+	camera.position.set(60, 140, 190); // traslado de la camara desde el origen de coordenadas
+	camera.lookAt(new THREE.Vector3(0,130/2,0));
+	//scene.add(cenital);
 	scene.add(camera);
 }
 
@@ -56,7 +58,6 @@ function setPersonalCamera (){
 		personalCamera.lookAt(scene.position);
 		scene.add(personalCamera);
 }
-
 
 function init() {
   // Crear el motor, la escena y la camara
@@ -79,8 +80,12 @@ function init() {
 
   //controlador de camara
   cameraController = new THREE.OrbitControls( camera, renderer.domElement);
-  cameraController.target.set(0,0,0);
+  cameraController.target.set(0,130/2,0);
   cameraController.noKeys = true;
+
+  // Luces
+  var luzAmbiente = new THREE.AmbientLight(0xFFFFFF, 0.9);
+  scene.add(luzAmbiente);
 
 
   window.addEventListener('resize', updateAspectRatio);
@@ -134,26 +139,66 @@ function loadScene() {
   // Cargar la escena con objetos
 
   // Materiales
-  material = new THREE.MeshBasicMaterial({ color: 'pink', solid: true });
+ 	material = new THREE.MeshBasicMaterial({ color: 'pink', wireframe: true });
 
 	document.getElementById('file-input').addEventListener('change', leerArchivo, false);
 
-  //scene.add( new THREE.AxisHelper(30) );
-  var paredes = [ path+'posx6.png',path+'negx6.png',
+	//scene.add( new THREE.AxisHelper(30) );
+
+	// Suelo adoquines
+	// var texturaSuelo = new THREE.TextureLoader().load(path+'pavimento2.jpg');
+    // texturaSuelo.magFilter = THREE.LinearFilter;
+    // texturaSuelo.minFilter = THREE.LinearFilter;
+    // texturaSuelo.repeat.set(10,10);
+	// texturaSuelo.wrapS = texturaSuelo.wrapT = THREE.MirroredRepeatWrapping;
+	// var materialSuelo = new THREE.MeshPhongMaterial({color:'white', map: texturaSuelo});
+	// var geoSuelo = new THREE.PlaneGeometry(400, 400, 10, 10);
+	// var suelo = new THREE.Mesh(geoSuelo, materialSuelo);
+	// suelo.rotation.x = -Math.PI/2;
+	// scene.add(suelo);
+
+	// Entorno habitacion 
+	var paredes = [ path+'posx6.png',path+'negx6.png',
                     path+'posy6.png',path+'negy6.png',
                     path+'posz6.png',path+'negz6.png'
                     ];
     var mapaEntorno = new THREE.CubeTextureLoader().load(paredes);
-  var shader = THREE.ShaderLib.cube;
-  shader.uniforms.tCube.value = mapaEntorno;
-  var matparedes = new THREE.ShaderMaterial({
-    fragmentShader: shader.fragmentShader,
-    vertexShader: shader.vertexShader,
+	var shader = THREE.ShaderLib.cube;
+	shader.uniforms.tCube.value = mapaEntorno;
+	var matparedes = new THREE.ShaderMaterial({
+		fragmentShader: shader.fragmentShader,
+		vertexShader: shader.vertexShader,
         uniforms: shader.uniforms,
-    side: THREE.BackSide
-  });
-  var habitacion = new THREE.Mesh( new THREE.CubeGeometry(400,400,400),matparedes);
-  scene.add(habitacion);
+		side: THREE.BackSide
+	});
+	var habitacion = new THREE.Mesh( new THREE.CubeGeometry(400,400,400),matparedes);
+	scene.add(habitacion);
+
+
+/*
+	const loader = new THREE.TextureLoader();
+	loader.load('images/edificios/morning.jpg' , function(texture)
+	{
+	 var fondo = new THREE.MeshBasicMaterial({ map:texture });
+	 set_city_backGround(fondo);
+	});
+
+	var tex = loader.load('images/edificios/asfalto.jpg' , function(texture)
+				{
+				 var fondo = new THREE.MeshBasicMaterial({ map:texture });
+				 var suelo = new THREE.PlaneGeometry(384,384,10,10);
+	 				asfalto = new THREE.Mesh(suelo, fondo);
+					asfalto.rotation.x = -Math.PI / 2;
+					asfalto.position.x = 192;
+					asfalto.position.z = 192;
+
+	 				scene.add(asfalto);
+				});
+
+		tex.wrapS = THREE.RepeatWrapping;
+		tex.wrapT = THREE.RepeatWrapping;
+		tex.repeat.set( 90, 90 );
+*/
 }
 
 function update(){
@@ -174,7 +219,7 @@ function enterPassenger(elevator, passenger, seat){
 	var newZPos = 2;
 	var newXPos = 0;
 	var newYPos = - 5 + 2*seat;
-
+	
 	new TWEEN.Tween(passenger.position)
 		 .to(passenger.position.clone().setZ(elevator.position.z).setX(elevator.position.x), 500).onComplete(
 			 function() {
@@ -208,6 +253,7 @@ function exitPassenger(elevator, passenger, seat){
 
 function leerArchivo(e) {
   var archivo = e.target.files[0];
+  console.log(archivo)
   if (!archivo) {
     return;
   }
@@ -218,59 +264,60 @@ function leerArchivo(e) {
   };
   lector.readAsText(archivo);
 }
+
 function generateBuilding() {
 
   for (var i=0; i <= 13; i++){
-    // Geometrias
-    var height = 10*i;
-    //var floor = new THREE.PlaneGeometry(50, 20);
-    var floor = new THREE.BoxGeometry(50,20,0.5);
-    var tex_to_modify = tex_default;
-    //material = new THREE.MeshBasicMaterial({ color: 'grey', wireframe: false, side: THREE.DoubleSide});
-    material = new THREE.MeshPhongMaterial({
-      color:  "rgb(194, 155, 97)",
-      opacity: 0.7,
-      transparent: true}); // color suelo
+		// Geometrias
+		var height = 10*i;
+		//var floor = new THREE.PlaneGeometry(50, 20);
+		var floor = new THREE.BoxGeometry(50,20,0.5);
+		var tex_to_modify = tex_default;
+		//material = new THREE.MeshBasicMaterial({ color: 'grey', wireframe: false, side: THREE.DoubleSide});
+		material = new THREE.MeshPhongMaterial({ 
+			color:  "rgb(194, 155, 97)", 
+			opacity: 0.7,
+			transparent: true}); // color suelo	
 
-    var floorMesh = new THREE.Mesh(floor, material);
-    floorMesh.rotation.x = -Math.PI / 2;
-    floorMesh.position.y = height;
+		var floorMesh = new THREE.Mesh(floor, material);
+		floorMesh.rotation.x = -Math.PI / 2;
+		floorMesh.position.y = height;
 
-    material = new THREE.MeshBasicMaterial({ color: 'pink', wireframe: true });
-    problem.add(floorMesh);
+		material = new THREE.MeshBasicMaterial({ color: 'pink', wireframe: true }); 
+		problem.add(floorMesh);
+		
 
+		if(i != 13){
+			
+			var wall = new THREE.PlaneGeometry(50, 10);
+			texturePared = new THREE.TextureLoader().load(path + 'pared'+ i+'.png'); // Añadir paredes fondo
+			materialPared = new THREE.MeshPhongMaterial({map: texturePared, side: THREE.DoubleSide});
+			var wallMesh = new THREE.Mesh(wall, materialPared);
+			wallMesh.position.y = height + 5;
+			wallMesh.position.z = -10;
+			problem.add(wallMesh);
+			
 
-    if(i != 13){
+			// Geometrias
+			// 512 x 512, size 300
+			// https://onlinenumbertools.com/convert-numbers-to-image
+			texture0 = new THREE.TextureLoader().load(path + i + '.png'); // Añadir numeros
+			materialNumero = new THREE.MeshPhongMaterial({map: texture0, side: THREE.DoubleSide});
 
-      var wall = new THREE.PlaneGeometry(50, 10);
-      texturePared = new THREE.TextureLoader().load(path + 'pared'+ i+'.png'); // Añadir paredes fondo
-      materialPared = new THREE.MeshPhongMaterial({map: texturePared, side: THREE.DoubleSide});
-      var wallMesh = new THREE.Mesh(wall, materialPared);
-      wallMesh.position.y = height + 5;
-      wallMesh.position.z = -10;
-      problem.add(wallMesh);
+			var wall = new THREE.PlaneGeometry(20, 10);
+			var wallMesh = new THREE.Mesh(wall, materialNumero);
+			wallMesh.rotation.y = Math.PI / 2;
+			wallMesh.position.y = height + 5;
+			wallMesh.position.x = -25;
 
-
-      // Geometrias
-      // 512 x 512, size 300
-      // https://onlinenumbertools.com/convert-numbers-to-image
-      texture0 = new THREE.TextureLoader().load(path + i + '.png'); // Añadir numeros
-      materialNumero = new THREE.MeshPhongMaterial({map: texture0, side: THREE.DoubleSide});
-
-      var wall = new THREE.PlaneGeometry(20, 10);
-      var wallMesh = new THREE.Mesh(wall, materialNumero);
-      wallMesh.rotation.y = Math.PI / 2;
-      wallMesh.position.y = height + 5;
-      wallMesh.position.x = -25;
-
-      problem.add(wallMesh);
-    }
-  }
+			problem.add(wallMesh);
+		}
+	}
 
 }
 
 function generatePassenger(planta, celda = 0){
-  // Geometrias
+	// Geometrias
 	var height = 1;
 	var passenger = new THREE.CylinderGeometry(0.5, 1, height, 64);
 	var head = new THREE.SphereGeometry(0.5, 32, 32);
@@ -305,7 +352,7 @@ function generatePassenger(planta, celda = 0){
 }
 
 function generateElevator(elevator, position_y, position_x, slow = true){
-  // Geometrias
+	// Geometrias
 	var height = 10;
 	var cube_elevator = new THREE.BoxGeometry(4, height, 4);
 	var texturaAscensor = new THREE.TextureLoader().load(path+'ascensor.jpg');
@@ -318,8 +365,10 @@ function generateElevator(elevator, position_y, position_x, slow = true){
 		//color = 'yellow'
 	}
 
-	var elevMesh = new THREE.Mesh(cube_elevator, mat);
+	//var mat = new THREE.MeshBasicMaterial({ color, wireframe: false });
 
+	var elevMesh = new THREE.Mesh(cube_elevator, mat);
+	
 	elevator.add(elevMesh);
 	elevator.position.x = position_x;
 	elevator.position.y = height/2 + position_y;
@@ -331,6 +380,7 @@ function readLine( ) {
 	if(!personalCamera){
 		//setPersonalCamera ();
 	}
+
 
   if(!contenido){
 		if(!notified){
@@ -344,7 +394,7 @@ function readLine( ) {
     return;
   }
   var tokens = lines[lineToRead].split(' ');
-  document.getElementById("actionInProgress").innerHTML = lines[lineToRead];
+
   lineToRead +=1;
   /* ACCIONES */
   if(tokens[1] == "GODOWN" || tokens[1] == "GOUP"){
@@ -499,13 +549,14 @@ function render(){
 
 
   var size = Math.min(window.innerWidth, window.innerHeight)/4;
-  /*renderer.setViewport(0, 0, size, size);
-  renderer.render( scene, cenital );
-*/
+  renderer.setViewport(0, 0, size, size);
+  //renderer.render( scene, cenital );
+
 	if(personalCamera){
 		renderer.setViewport(window.innerWidth-(size*2), window.innerHeight-(size*2), size*2, size*2);
 	  renderer.render( scene, personalCamera );
 	}
+	
 }
 
 function updateAspectRatio(){
@@ -516,12 +567,14 @@ function updateAspectRatio(){
 	var size = Math.min(window.innerWidth, window.innerHeight)/4;
 
 	if(ar>1){
+		
 		if(personalCamera){
 		  personalCamera.left = l*ar;
 		  personalCamera.right = r*ar;
 		  personalCamera.bottom = b;
 		  personalCamera.top = t;
 		}
+		
 	  camera.left = l*ar;
 	  camera.right = r*ar;
 	  camera.bottom = b;
@@ -534,6 +587,7 @@ function updateAspectRatio(){
 		  personalCamera.bottom = b/ar;
 		  personalCamera.bottom = t/ar;
 		}
+
 	  camera.left = l;
 	  camera.right = r;
 	  camera.bottom = b/ar;
@@ -548,4 +602,17 @@ function updateAspectRatio(){
 		personalCamera.updateProjectionMatrix();
 	}
 	render();
+}
+
+function set_city_backGround(fondo){
+	var back = new THREE.PlaneGeometry(384,185,10,10);
+	backX = new THREE.Mesh(back, fondo);
+  backZ = new THREE.Mesh(back, fondo);
+  backX.rotation.y = Math.PI / 2;
+  backX.position.y = 50;
+  backZ.position.y = 50;
+  backZ.position.x = 192;
+  backX.position.z = 192;
+  scene.add(backX);
+	scene.add(backZ);
 }
